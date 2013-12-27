e73f1936c7e6b7129c22c0e4ac8f08776f4d92cf
Andrei Bosco <andreibosco@gmail.com>
2013-12-27 19:31:49 -0300

    carregando valores de specular, ambient e emission dos materiais;

--- object.cpp
+++ object.cpp
@@ -52,16 +52,38 @@ void Object::set_float4(float f[4], float a, float b, float c, float d)
 void Object::aplicarMaterial(const aiMaterial *mtl)
 {
     float c[4];
-    aiColor4D diffuse;
+    aiColor4D diffuse, specular, ambient, emission;
 
     set_float4(c, 0.8f, 0.8f, 0.8f, 1.0f);
     if (AI_SUCCESS == aiGetMaterialColor(mtl, AI_MATKEY_COLOR_DIFFUSE, &diffuse))
     {
         color4_to_float4(&diffuse, c);
     }
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, c);
+
+    set_float4(c, 0.0f, 0.0f, 0.0f, 1.0f);
+    if (AI_SUCCESS == aiGetMaterialColor(mtl, AI_MATKEY_COLOR_SPECULAR, &specular))
+    {
+        color4_to_float4(&specular, c);
+    }
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, c);
+
+    set_float4(c, 0.2f, 0.2f, 0.2f, 1.0f);
+    if (AI_SUCCESS == aiGetMaterialColor(mtl, AI_MATKEY_COLOR_AMBIENT, &ambient))
+    {
+        color4_to_float4(&ambient, c);
+    }
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, c);
+
+    set_float4(c, 0.0f, 0.0f, 0.0f, 1.0f);
+    if (AI_SUCCESS == aiGetMaterialColor(mtl, AI_MATKEY_COLOR_EMISSIVE, &emission))
+    {
+        color4_to_float4(&emission, c);
+    }
+    glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION, c);
 
     //float diffuse[4] = {0.0, 1.0, 0.0, 0.0};
-    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, c);
+
 }
 
 void Object::render(const aiScene *sc, const aiNode* nd)
